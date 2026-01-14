7234b98b66b6b4a267750d1bde87382a5c116961
yjcha069kr <yjcha069kr@gmail.com>
2026-01-14 18:00:02 +0900

    260114 반응형

---
+++ js/hamburger.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const menu = document.querySelector(".menu");
+
+  hamburger.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
+});
